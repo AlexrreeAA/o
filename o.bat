6e3818fa4244b0ba7ed686cdbbb20854bd25453b
AlexrreeA <alexrreef5555gg@gmail.com>
2026-04-26 20:00:27 +0500

    o

---
+++ o.bat
@@ -0,0 +1,18 @@
+@echo off                   
+echo _________________________
+echo !     _____________      !
+echo !     !           !      !
+echo !     !           !      !
+echo !     !           !      !
+echo !     !           !      !
+echo !     !           !      !
+echo !     !           !      !
+echo !     !           !      !
+echo !     !           !      !
+echo !     !           !      !
+echo !     !           !      !
+echo !     !___________!      !
+echo !                        !
+echo !                        !
+echo !________________________!            
+pause
